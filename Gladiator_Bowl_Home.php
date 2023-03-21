61924f7b8b04b31df9c865c9eaafb6ef988f3a30
Doctor102 <87093410+Doctor102@users.noreply.github.com>
2023-03-21 23:06:47 +0000

    Add files via upload

---
+++ Gladiator_Bowl_Home.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gladiator Bowl - Home</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+		}
+		#banner {
+			background-color: #100d2c;
+			color: white;
+			height: 100px;
+			display: flex;
+			align-items: center;
+			padding: 0 20px;
+			justify-content: space-between;
+		}
+		#logo img {
+			height: 60px;
+			width: auto;
+		}
+		#links {
+			display: flex;
+			flex: 1;
+			justify-content: space-between;
+      		margin-left: 80px;
+			margin-right: 80px;
+		}
+		#links a {
+			color: white;
+			text-decoration: none;
+		}
+		#links a:hover {
+			color: #fcad36;
+		}
+		#main {
+			text-align: center;
+			padding: 50px 0;
+		}
+		#main h1 {
+			font-size: 36px;
+			color: black;
+			margin: 0 0 40px 0;
+		}
+		#main h4 {
+			font-size: 20px;
+			color: black;
+			margin: 0;
+		}
+	</style>
+</head>
+<body>
+	<div id="banner">
+		<div id="logo">
+			<img src="Gladiator_Bowl_Home_Banner_Logo_Ver2.jpg" alt="Gladiator Bowl Logo">
+		</div>
+		<div id="links">
+			<a href="#">Home</a>
+			<a href="#">Find a Job</a>
+			<a href="#">Post a Job</a>
+			<a href="#">About Us</a>
+			<a href="#">Contact Us</a>
+			<a href="#">Log In</a>
+			<a href="#">Sign Up</a>
+		</div>
+	</div>
+	<div id="main">
+		<h1>Join the Ultimate Fighting Network</h1>
+		<h4>Connect with other MMA fighters from around the world, showcase your skills, and grow your career.</h4>
+	</div>
+</body>
+</html>
