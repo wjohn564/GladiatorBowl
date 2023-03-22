c981c9ca65c506bda885a19c3e911a906c3c9a3f
Doctor102 <87093410+Doctor102@users.noreply.github.com>
2023-03-22 22:55:42 +0000

    Prototype homepage

--- Gladiator_Bowl_Contact_Us.php
+++ Gladiator_Bowl_Contact_Us.php
@@ -1,62 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Gladiator Bowl - Contact Us</title>
-    <style>
-        body {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-        }
-
-        #banner {
-            background-color: #100d2c;
-            color: white;
-            height: 100px;
-            display: flex;
-            align-items: center;
-            padding: 0 20px;
-        }
-
-        #logo img {
-            height: 60px;
-            width: auto;
-        }
-
-        #links {
-            margin-left: auto;
-            display: flex;
-            align-items: center;
-            flex-grow: 1;
-        }
-
-        #links a {
-            color: white;
-            text-decoration: none;
-            margin-right: 20px;
-            text-align: center;
-            flex-grow: 1;
-        }
-
-        #links a:hover {
-            color: #fcad36;
-        }
-
+	<title>Gladiator Bowl - Contact Us</title>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			font-family: Arial, sans-serif;
+		}
+		#banner {
+			background-color: #100d2c;
+			color: white;
+			height: 100px;
+			display: flex;
+			align-items: center;
+			padding: 0 20px;
+		}
+		#logo img {
+			height: 60px;
+			width: auto;
+		}
+		#links {
+			margin-left: auto;
+			display: flex;
+			align-items: center;
+			flex-grow: 1;
+		}
+		#links a {
+			color: white;
+			text-decoration: none;
+			margin-right: 20px;
+			text-align: center;
+			flex-grow: 1;
+		}
+		#links a:hover {
+			color: #fcad36;
+		}
         h1 {
             margin-top: 40px;
             margin-left: 20px;
         }
-
         h2 {
             margin-left: 20px;
         }
-
         ul {
             list-style-type: none;
             margin-left: 40px;
             padding: 0;
         }
-
         li:before {
             content: "\2022";
             color: #fcad36;
@@ -64,29 +55,29 @@
             width: 1em;
             margin-left: -1em;
         }
-    </style>
+	</style>
 </head>
 <body>
-<div id="banner">
-    <div id="logo">
-        <img src="Gladiator_Bowl_Home_Banner_Logo_Ver2.jpg" alt="Gladiator Bowl Logo">
-    </div>
-    <div id="links">
-        <a href="#">Home</a>
-        <a href="#">Find a Job</a>
-        <a href="#">Post a Job</a>
-        <a href="#">About Us</a>
-        <a href="#">Contact Us</a>
-        <a href="#">Log In</a>
-        <a href="#">Sign Up</a>
-    </div>
-</div>
-<h1>Contact Us</h1>
-<h2>Contact Information</h2>
-<ul>
-    <li>Email: info@gladiatorbowl.com</li>
-    <li>Phone: (555) 555-5555</li>
-    <li>Address: 123 Main Street, Anytown, USA</li>
-</ul>
+	<div id="banner">
+		<div id="logo">
+			<img src="images/Gladiator_Bowl_Home_Banner_Logo_Ver2.jpg" alt="Gladiator Bowl Logo">
+		</div>
+		<div id="links">
+			<a href="Gladiator_Bowl_Home.php">Home</a>
+			<a href="#">Find a Job</a>
+			<a href="#">Post a Job</a>
+			<a href="Gladiator_Bowl_About_Us.php">About Us</a>
+			<a href="Gladiator_Bowl_Contact_Us.php">Contact Us</a>
+			<a href="login.php">Log In</a>
+			<a href="registration.php">Sign Up</a>
+		</div>
+	</div>
+    <h1>Contact Us</h1>
+    <h2>Contact Information</h2>
+    <ul>
+        <li>Email: info@gladiatorbowl.com</li>
+        <li>Phone: (555) 555-5555</li>
+        <li>Address: 123 Main Street, Anytown, USA</li>
+    </ul>
 </body>
 </html>
